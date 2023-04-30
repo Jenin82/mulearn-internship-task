67acf5a914d2cb167fdc52852806c8edf8a8b09b
Jenin82 <jenin8282@gmail.com>
2023-04-30 17:39:09 +0530

    signup redirect

--- user/views.py
+++ user/views.py
@@ -42,19 +42,20 @@ def logoutPage(request):
   return render(request, "user/login.html", context)
 
 def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('home')
-    else:
-        form = UserCreationForm()
-    return render(request, 'user/signup.html', {'form': form})
-  
+  if request.user.is_authenticated:
+    return redirect('home')
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      username = form.cleaned_data.get('username')
+      raw_password = form.cleaned_data.get('password1')
+      user = authenticate(username=username, password=raw_password)
+      login(request, user)
+      return redirect('home')
+  else:
+      return redirect('signup')
+
 def createTodo(request):
   todo = Todo()
   context = {'todo': todo}
