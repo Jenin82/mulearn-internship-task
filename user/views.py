ff9c6f98d8fb64b203a524819b4acfa7318c42ba
Jenin82 <jenin8282@gmail.com>
2023-04-30 22:38:38 +0530

    fix

--- user/views.py
+++ user/views.py
@@ -41,10 +41,10 @@ def logoutPage(request):
 
 
 def signup(request):
+  form = UserCreationForm(request.POST)
   if request.user.is_authenticated:
     return redirect('home')
   if request.method == 'POST':
-    form = UserCreationForm(request.POST)
     if form.is_valid():
       form.save()
       username = form.cleaned_data.get('username')
