92b8580ae8beb6c750589c70244a4fa2cffb7fce
Adnan Kattekaden <adnankattekaden2020@gmail.com>
2023-10-08 18:25:15 +0530

    minor fix

--- task/urls.py
+++ task/urls.py
@@ -20,6 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user.urls')),
-    path('api/', include('user.api.urls')),
+    path('api/v1/mulearn-task/', include('user.api.urls')),
     
 ]
--- user/urls.py
+++ user/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-		path('', views.homePage, name="home"),
-		path('login/', views.loginPage, name="login"),
-		path('logout/', views.logoutPage, name="logout"),
+    path('', views.homePage, name="home"),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutPage, name="logout"),
     path('signup/', views.signup, name="signup"),
     path('create-todo/', views.createTodo, name="create-todo"),
     path('todo/<str:pk>/', views.todo, name="todo"),
     path('todo/<str:pk>/completed', views.statusCompleted, name="completed"),
     path('todo/<str:pk>/inprogress', views.statusInprogress, name="inprogress"),
 ]
-
-
